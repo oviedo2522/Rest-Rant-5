dfe1767a3e71edddb10c83ae72e1f86d33d90659
oviedo2522 <oviedo2522@gmail.com>
2024-05-28 15:41:06 -0700

    Errors

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
 const Def = require('../default')
 
-function index (data) {
+function index(data) {
   let placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6">
         <h2>
-          <a href={`/places/${index}`} >
+          <a href={`/places/${place.id}`} >
             {place.name}
           </a>
         </h2>
@@ -22,12 +22,12 @@ function index (data) {
   })
   return (
     <Def>
-        <main>
-            <h1>Places to Rant or Rave About</h1>
-            <div className="row">
-              {placesFormatted}
-            </div>
-        </main>
+      <main>
+        <h1>Places to Rant or Rave About</h1>
+        <div className="row">
+          {placesFormatted}
+        </div>
+      </main>
     </Def>
   )
 }
