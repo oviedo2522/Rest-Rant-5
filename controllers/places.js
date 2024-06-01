55c6d6fd0491b4d7aa500904c0e0597acce10bd4
oviedo2522 <oviedo2522@gmail.com>
2024-06-01 14:27:05 -0700

    Comments

--- controllers/places.js
+++ controllers/places.js
@@ -35,7 +35,9 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+    console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -43,6 +45,28 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,25 +1,38 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form(data) {
-  let message = ''
-  if (data.message) {
-    message = (
-      <h4 className="alert-danger">
-        {data.message}
-      </h4>
-    )
+function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
   }
   return (
     <Def>
       <main>
-        <h1>Add a New Place</h1>
-        {message}
-        ...
+        <div className="row">
+          ...
+        </div>
+        <hr />
+        <h2>Comments</h2>
+        {comments}
       </main>
     </Def>
   )
 }
 
-module.exports = new_form
-
+module.exports = show
