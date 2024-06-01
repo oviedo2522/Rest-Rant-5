4fd90790d33e2930c3148b0e508e3d05c49ae7ea
oviedo2522 <oviedo2522@gmail.com>
2024-06-01 13:58:30 -0700

    New Places

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,34 +1,25 @@
 const React = require('react')
 const Def = require('../default')
 
-function show({ place, id }) {
+function new_form(data) {
+  let message = ''
+  if (data.message) {
+    message = (
+      <h4 className="alert-danger">
+        {data.message}
+      </h4>
+    )
+  }
   return (
     <Def>
       <main>
-        <div className="row">
-          <div className="col-sm-6">
-            <img src={data.place.pic} alt={data.place.name} />
-            <h3>
-              Located in {data.place.city}, {data.place.state}
-            </h3>
-          </div>
-          <div className="col-sm-6">
-            ...
-            <h2>
-              Description
-            </h2>
-            <h3>
-              {data.place.showEstablished()}
-            </h3>
-            <h4>
-              Serving {data.place.cuisines}
-            </h4>
-            ...
-          </div>
+        <h1>Add a New Place</h1>
+        {message}
+        ...
       </main>
     </Def>
-
   )
 }
 
-module.exports = show
+module.exports = new_form
+
