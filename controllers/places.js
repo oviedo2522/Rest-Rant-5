bdab5ab30eb19cb526f6b113ec3b07e7e70ae8b7
oviedo2522 <oviedo2522@gmail.com>
2024-06-04 19:00:13 -0700

    New Comments

--- controllers/places.js
+++ controllers/places.js
@@ -67,34 +67,28 @@ router.post('/:id/comment', (req, res) => {
   })
 })
 
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
-})
-
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
-
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
 
 module.exports = router
 
